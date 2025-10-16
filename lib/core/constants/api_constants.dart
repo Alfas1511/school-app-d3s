f31840e04ae7a355413345492741182753c79ec7
Alfas Salim <alfassalim123@gmail.com>
2025-10-16 14:46:01 +0530

    api added for student attendance

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -10,4 +10,5 @@ class ApiConstants {
   static const String logout = "$baseUrl/parent/logout";
   static const String parentProfile = "$baseUrl/parent/profile";
   static const String studentList = "$baseUrl/parent/students-list";
+  static const String studentAttendance = "$baseUrl/parent/student-attendance";
 }
