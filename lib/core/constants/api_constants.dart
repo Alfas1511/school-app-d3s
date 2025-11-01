1a11383139a1995883b6e1be23c7d7678b9adbc0
Alfas Salim <alfassalim123@gmail.com>
2025-11-01 17:34:49 +0530

    api base urls mentioned

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,9 +1,10 @@
 // lib/core/constants/api_constants.dart
 class ApiConstants {
-  // static const String baseUrl = "http://127.0.0.1:8000/api";
-  static const String baseUrl = "http://10.0.2.2:8000/api";
-  // static const String baseUrl =
-  //     "https://mediumorchid-falcon-472579.hostingersite.com/api";
+  // static const String baseUrl = "http://127.0.0.1:8000/api"; // Localhost
+  // static const String baseUrl = "http://192.168.1.19:8000/api"; // Physical Device
+  // static const String baseUrl = "http://10.0.2.2:8000/api";  // Android Emulator
+  static const String baseUrl =
+      "https://mediumorchid-falcon-472579.hostingersite.com/api"; // Server
 
   // Endpoints
   static const String login = "$baseUrl/parent/login";
